9ccd49a50c15ec83006044792a026893f2c3d973
Your Name <Andrew112195@gmail.com>
2023-02-16 17:45:37 -0500

    test

--- codenexus/src/main/java/com/backend/codenexus/entity/MessagesEntity.java
+++ codenexus/src/main/java/com/backend/codenexus/entity/MessagesEntity.java
@@ -1,4 +1,25 @@
 package com.backend.codenexus.entity;
+import com.backend.codenexus.model.Messages;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "messsage")
 
 public class MessagesEntity {
+    
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private MessagesEntity userId;
+
+    
 }
