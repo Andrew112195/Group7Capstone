d53c504b6e86be941d442cf1bb43e5802527ce87
Your Name <Andrew112195@gmail.com>
2023-02-16 19:08:44 -0500

    Main

--- codenexus/src/main/java/com/backend/codenexus/model/Subscription.java
+++ codenexus/src/main/java/com/backend/codenexus/model/Subscription.java
@@ -2,6 +2,13 @@ package com.backend.codenexus.model;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Subscription {
     private long id;
     private long userId;
