b8593921c2f554a920288ad3e4002473c7dfe4af
berlyPaul360 <berlympaul@gmail.com>
2023-03-11 11:21:09 -0500

    added scripts

---
+++ codenexus/src/main/resources/Sql Scripts to Run program/scripts.sql
@@ -0,0 +1,9 @@
+INSERT INTO course (description, is_complete,pre_requisites,price,title) VALUES ('fun', TRUE, TRUE, 14, 'funtimes');
+INSERT INTO course (description, is_complete,pre_requisites,price,title) VALUES ('very fun', TRUE, TRUE, 18, 'awesome times');
+INSERT INTO course (description, is_complete,pre_requisites,price,title) VALUES ('very expensive', TRUE, TRUE, 132, 'tough times');
+INSERT INTO cohort (name) VALUES ('Cohort A');
+INSERT INTO cohort (name) VALUES ('Cohort B');
+INSERT INTO cohort (name) VALUES ('Cohort C');
+INSERT INTO user_type (type_name) VALUES ('Student');
+INSERT INTO user_type (type_name) VALUES ('Instructor');
+INSERT INTO user_type (type_name) VALUES ('Admin');
