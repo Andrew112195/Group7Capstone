80a08d3b0aa7d79d84e764828e826655d42c45b2
Andrew <Andrew112195@gmail.com>
2023-04-11 19:42:25 -0400

    fixed bug on update user

--- codenexus/src/main/java/com/backend/codenexus/dao/UserDao.java
+++ codenexus/src/main/java/com/backend/codenexus/dao/UserDao.java
@@ -1,16 +1,10 @@
 package com.backend.codenexus.dao;
 
-import com.backend.codenexus.entity.UserCourseEntity;
 import com.backend.codenexus.entity.UserEntity;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
